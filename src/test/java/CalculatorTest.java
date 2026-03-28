12d56b58fac2102fda03080bd08dcd706b828124
GOPALREDYT999 <gopalreddyt123@gmail.com>
2026-03-28 15:02:57 +0530

    Added unit tests

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+  Calculator calc = new Calculator();
+
+  @Test
+  void testAdd() {
+    assertEquals(15, calc.add(10, 5));
+  }
+
+  @Test
+  void testSubtract() {
+    assertEquals(5, calc.subtract(10, 5));
+  }
+
+  @Test
+  void testMultiply() {
+    assertEquals(50, calc.multiply(10, 5));
+  }
+
+  @Test
+  void testDivide() {
+    assertEquals(2, calc.divide(10, 5));
+  }
+
+  @Test
+  void testDivideByZero() {
+    assertEquals(0, calc.divide(10, 0));
+  }
+}
